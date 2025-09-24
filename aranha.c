7d45c5b29f20a195621a59ecc1542fd6f5f1ab24
Antonio Neto <antonio.neto.cost@gmail.com>
2025-09-24 10:33:53 -0300

    musica no jogo
    sim sim, agora temos musica no jogo,, talvez isso gere alguns problemas futuros, ms ja estou pensando sobre

--- aranha.c
+++ aranha.c
@@ -1290,12 +1290,7 @@ void AtualizarTempo() {
         quedaFatal = true;   // Marca a queda como fatal (não pode ser salva com a teia).
     }
 }
-
-// =================================================================================
-// FUNÇÃO PRINCIPAL (main)
-// O ponto de entrada do programa e o loop principal do jogo.
-// =================================================================================
-int main(){
+DWORD WINAPI Jogo(){
     // Inicializa o gerador de números aleatórios com a hora atual, para que os spawns sejam diferentes a cada jogo.
     srand(time(NULL));
     // Configura o console para exibir corretamente caracteres UTF-8 (como os usados no prédio).
@@ -1335,6 +1330,45 @@ int main(){
         // Pausa o jogo por um curto período para controlar a velocidade.
         Sleep(ATRASO_TIQUE);
     }
+
+    return 0;
+}
+
+DWORD WINAPI Musica(){
+    int frequencia_parte1[] = {330, 370, 392, 392, 370, 330, 0, 330, 370, 392, 330, 370, 392};
+    int duracao_parte1[] = {150, 150, 300, 150, 150, 300, 100, 150, 150, 150, 150, 150, 300};
+
+    int frequencia_parte2[] = {523, 523, 493, 440, 440, 392};
+    int duracao_parte2[] = {300, 150, 150, 300, 150, 450};
+
+    while(1){
+        for(int i = 0; i < 13; i++){
+            Beep(frequencia_parte1[i], duracao_parte1[i]);
+            Sleep(50);
+        }
+
+        for(int i = 0; i < 6; i++){
+            Beep(frequencia_parte2[i], duracao_parte2[i]);
+            Sleep(50);
+        }
+    }
+
+    return 0;
+}
+
+// =================================================================================
+// FUNÇÃO PRINCIPAL (main)
+// O ponto de entrada do programa e o loop principal do jogo.
+// =================================================================================
+int main(){
+    HANDLE thread1;
+    HANDLE thread2;
+
+    thread1 = CreateThread(NULL, 0, Jogo, NULL, 0, NULL);
+    thread2 = CreateThread(NULL, 0, Musica, NULL, 0, NULL);
+
+    WaitForSingleObject(thread1, INFINITE);
+    WaitForSingleObject(thread2, INFINITE);
     return 0; // Fim do programa (teoricamente nunca alcançado devido ao loop infinito).
 }
 
